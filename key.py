bda061d2923261282a9cb9e4ab4e033ee9a2c22c
MaryLover <755107412@qq.com>
2018-09-17 21:40:03 +0800

    dic and set

---
+++ key.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+score = {'高数':95,'英语':72,'信号':80,'模电':91}
+print('score[\'高数\']=',score['高数'])
+print('score[\'英语\']=',score['英语'])
+print('score[\'信号\']=',score['信号'])
+print('score[\'模电\']=',score['模电'])
+
---
+++ set.py
@@ -0,0 +1,5 @@
+set1 = set([1,2,3,4,5])
+print(set1)
+set2 = set([2,3,4,6,8])
+print(set1|set2)
+print(set1&set2)
