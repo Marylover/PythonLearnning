f19fb21128fb15369899f5c15c761e08caf0dc50
dong <755107412@qq.com>
2018-09-18 23:45:42 +0800

    修改

--- multiFactor.py
+++ multiFactor.py
@@ -4,4 +4,7 @@ def multiFactor(a,b,*c):
     for factors in c :
         s = s*factors 
     return s
-print(multiFactor(3,5,5,2))
+a = (1,3,5,7)
+b = {2,4,6,8}
+print(multiFactor(*a)) ## tuple 可作为参数传入函数做参数
+print(multiFactor(*b)) ## dict 可作为参数穿入函数做参数
