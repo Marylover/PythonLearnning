0a5f11809eb9d56080462d4c52c234a5c0c7cd31
dong <755107412@qq.com>
2018-10-01 20:08:30 +0800

    AddMethod

--- oop.py
+++ oop.py
@@ -4,9 +4,19 @@ class Student(object):  #学习python第一个面向对象程序
         self.score = score 
     def print_score(self):
         print('%s: %s'%(self.name,self.score))
+    def get_grade(self):  #为Student类增加一个方法
+        if self.score < 60:
+            return 'D'
+        elif self.score <= 80:
+            return 'C'
+        elif self.score <= 85:
+            return 'B'
+        else:
+            return 'A'
 
 mary  = Student('Mary',90)
-jack = Student('Jack',95)
+jack = Student('Jack',70)
 mary.print_score()
 jack.print_score()
-    
+print(mary.name,mary.get_grade())
+print(jack.name,jack.get_grade())
