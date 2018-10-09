4034711ee181f52dc5a71f3bda0dda4fe727ec38
dong <755107412@qq.com>
2018-10-09 23:32:17 +0800

    修改

--- all_files.py
+++ all_files.py
@@ -1,6 +1,6 @@
 import os
-for root, dirs, files in os.walk(".", topdown=False):
-#   for name in files:
-#      print(os.path.join(root, name))
+for root, dirs, files in os.walk(".", topdown=False):  #root:文件夹路径，dirs:文件夹名，files:文件名
+    for name in files:
+        print(os.path.join(root, name))
     for name in dirs:
         print(os.path.join(root, name))
